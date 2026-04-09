79afa4acb4e7972f4ecbcbee74764c9e2c5f0ecc
gautam-vaishnav016 <gautam.vaishnav016@gmail.com>
2026-04-09 12:05:14 +0530

    main.jsx file  updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,80 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { lazy, Suspense } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App";
+import Error from "./components/Error";
+import "./index.css";
+import Loading from "./components/Loading";
+import ProductLayout from "./Layout/ProductLayout";
+// Lazy load components for code splitting
+const ProductList = lazy(() => import("./components/ProductList"));
+const Home = lazy(() => import("./pages/Home"));
+const ProductDetailPage = lazy(() => import("./pages/ProductDetailPage"));
+const CartPage = lazy(() => import("./pages/CartPage"));
+const CheckoutPage = lazy(() => import("./pages/CheckoutPage"));
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// Define application routes with lazy loading and error handling
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: (
+          <Suspense fallback={<Loading />}>
+            <Home />
+          </Suspense>
+        ),
+      },
+      {
+        path: "products",
+        element: <ProductLayout />,
+        errorElement: <Error />,
+        children: [
+          {
+            index: true,
+            element: (
+              <Suspense fallback={<Loading />}>
+                <ProductList />
+              </Suspense>
+            ),
+          },
+          {
+            path: ":id",
+            element: (
+              <Suspense fallback={<Loading />}>
+                <ProductDetailPage />
+              </Suspense>
+            ),
+          },
+          {
+            path: "*",
+            element: <Error />,
+          },
+        ],
+      },
+      {
+        path: "cart",
+        element: (
+          <Suspense fallback={<Loading />}>
+            <CartPage />
+          </Suspense>
+        ),
+      },
+      {
+        path: "checkout",
+        element: (
+          <Suspense fallback={<Loading />}>
+            <CheckoutPage />
+          </Suspense>
+        ),
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <RouterProvider router={appRouter} />,
+);
